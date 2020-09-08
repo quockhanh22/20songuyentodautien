2d19c6dc33b182d10deee4809bf65517d8a74783
quockhanh22 <anhkhanh11761@gmail.com>
2020-09-08 22:31:47 +0700

    20songuyentodautien
    20songuyentodautien

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace hienthi20songuyentodautien
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("20 số nguyên tố đầu tiên là:");
+            int index = 0;
+            for (int i = 2; i < 200; i++)
+            {
+                bool check = true;
+                for (int count = 2; count <= Math.Sqrt(i); count++)
+                {
+                    if (i % count == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                }
+                if (check)
+                {
+                    index = index + 1;
+                    if (index <= 20)
+                    {
+                        Console.WriteLine(i + "\t");
+                    }
+                }
+            }
+        }
+    }
+}
